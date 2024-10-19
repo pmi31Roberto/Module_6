3c3a2874a68aeee1783a28a2caa9d966ef04f077
Роберт <dificalabc@gmail.com>
2024-10-20 02:24:58 +0500

    one

---
+++ draft.py
@@ -0,0 +1,35 @@
+# class Human:
+#     head = True
+#     _legs = True
+#     arms = True
+#
+#     def say_hello(self):
+#         print(f'Зравствуйте')
+#
+#     def about(self):
+#         print(self.head)
+#         print(self._legs)
+#         print(self.arms)
+#
+#
+# class Student(Human):
+#     _legs = False
+#
+#
+# class Teacher(Human):
+#     pass
+#
+#
+# human = Human()
+# human.about()  # Выведет: True, True, True
+# print()
+# student = Student()
+# student.about()  # Выведет: True, False, True
+# print()
+# teacher = Teacher()
+# teacher.about()  # Выведет: True, True, True
+
+a = 'sasdAAA'
+a.lower()
+print('BLACK'.lower())
+print('black' == 'black')
---
+++ module_6_2.py
@@ -0,0 +1,55 @@
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner, _model, _color, _engine_power):
+        self.owner = owner
+        self._model = _model
+        self._engine_power = _engine_power
+        self._color = _color
+
+    def get_model(self):
+        print(f"Модель: {self._model}")
+
+    def get_horsepower(self):
+        print(f"Мощность двигателя: {self._engine_power}")
+
+    def get_color(self):
+        print(f"Цвет: {self._color}")
+
+    def print_info(self):
+        self.get_model()
+        self.get_horsepower()
+        self.get_color()
+        print(f"Владелец: {self.owner}")
+
+    def set_color(self, new_color):
+        flag = False
+        for i in self.__COLOR_VARIANTS:
+            if new_color.lower() == i.lower():
+                flag = True
+                break
+        if flag:
+            self._color = new_color
+        else:
+            print(f"Нельзя сменить цвет на {new_color}")
+
+
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle1.print_info()
